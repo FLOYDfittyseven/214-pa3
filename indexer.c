e1275147f71be5c895c9dcac2758c65990fb62a6
FLOYDfittyseven <FLOYDfittyseven@users.noreply.github.com>
2015-03-08 18:01:22 -0400

    Update indexer.c

--- indexer.c
+++ indexer.c
@@ -1,1 +1,34 @@
-#include "indexer.h"
+#include "record.h"
+#include <string.h>
+
+int recordCompare(struct record *arg1, struct record *arg2)
+{
+  if(arg1.tCount > arg2.tCount)
+  {
+    return 1;
+  }
+  else if (arg1.tCount < arg2.tCount)
+  {
+    return -1;
+  }
+  else
+  {
+    return 0;
+  }
+  
+  int x = strcmp(arg1.filename, arg2.filename);
+  
+  if(x < 0)
+  {
+    return 1;
+  }
+  else if (x > 0)
+  {
+    return -1;
+  }
+  else
+  {
+    return 0;
+  }
+
+}
