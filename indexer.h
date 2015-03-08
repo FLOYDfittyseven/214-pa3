1dfb88f3af7a7aab0285558f470e6e270c093333
FLOYDfittyseven <FLOYDfittyseven@users.noreply.github.com>
2015-03-08 14:28:26 -0400

    Update indexer.h

--- indexer.h
+++ indexer.h
@@ -4,3 +4,10 @@ struct record {
 	int tCount;
 };
 
+/* recordCompare compares two records. It compares them first by
+ * count, then by filename if the counts are equal. It returns
+ * -1 if arg2 goes before arg1 in the sorted-list, 1 if arg1 goes
+ * before arg2 in the sorted-list, and 0 if they are identical.
+ */
+int recordCompare (struct record *arg1, struct record *arg2);
+
